c719662e025ec7c3220061e0f5b154cd19714b63
root <root@ip-172-31-21-224.us-east-2.compute.internal>
2018-02-25 20:19:25 +0000

    adding site files

---
+++ breveo/scraper-youtube-captions/index.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+ini_set('max_execution_time', 300);
+include('xml2json.php');
+include('simple_html_dom.php');
+include('closed-captions.php');
+
+//getClosedCaptionsForVideo("bxHEZU0AFcQ",1,AIzaSyAZrDY528MDsbOJdkpx2ZxcVXAmeSII1YE)
+?>
